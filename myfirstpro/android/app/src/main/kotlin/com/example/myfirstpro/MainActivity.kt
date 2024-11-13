74afbcb7b326df53478bfd0cf4d8bc99ebfa11fc
Abderrahmane Bari <bariabdo1317@gmail.com>
2024-11-13 14:21:26 +0100

    add

---
+++ myfirstpro/android/app/src/main/kotlin/com/example/myfirstpro/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.myfirstpro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ myfirstpro/lib/main.dart
@@ -0,0 +1,51 @@
+
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Xapp(),
+    );
+  }
+}
+
+class Xapp extends StatefulWidget {
+  @override
+  State<Xapp> createState() => _XappState();
+}
+
+class _XappState extends State<Xapp> {
+  bool status = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Abderrahman",
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+        ),
+      ),
+      backgroundColor: Colors.amber[600],
+      body: Container(
+          child: Column(
+        children: [
+          SwitchListTile(
+              value: status,
+              onChanged: (val) {
+                setState(() {
+                  status = val;
+                });
+              })
+        ],
+      )),
+    );
+  }
+}
